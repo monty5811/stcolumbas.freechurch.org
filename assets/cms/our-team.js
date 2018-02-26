1b47218bcd8db7002afb1e49b43868db376eb43f
Dean Montgomery <montgomery.dean97@gmail.com>
2018-02-26 23:25:35 +0000

    update previews

--- assets/cms/our-team.js
+++ assets/cms/our-team.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 function members(team) {
-  return team.getIn(['data', 'members']).map(function(member) {
-    return <div className="col-xs-6 col-sm-4 col-md-3 col-lg-2">
+  return team.getIn(['data', 'members']).map(function(member, idx) {
+    return <div className="col-xs-6 col-sm-4 col-md-3 col-lg-2" key={idx}>
       <div className="thumbnail"><img src={member.get('picture')}/></div>
       <div className="caption">
         <h3>{ member.get('name') }</h3>
@@ -22,19 +22,21 @@ const OurTeamPreview = createClass({
           <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
             <h2 className="heading">{ team.getIn(['data', 'title']) }</h2>
           </div>
-          <div className="our-staff">
+          <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+          </div>
+        </div>
+        <div className="our-staff">
           <div className="container">
             <div className="row">
               { members(team) }
             </div>
-          </div>
           </div>
         </div>
       </div>;
     });
 
     return <div className="content">
-      <h1>{ this.props.widgetFor('heading') }</h1>
+      <h1>{ heading }</h1>
       <div className="container">
         <div className="row">
           <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
@@ -43,8 +45,8 @@ const OurTeamPreview = createClass({
           <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
             { entry.getIn(['data', 'intro', 'right']) }
           </div>
-          { rows }
         </div>
+        { rows }
       </div>
     </div>;
   }
