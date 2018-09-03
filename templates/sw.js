e6cf411c7e3a02ebb7087199c8bac98368fc7f2f
Dean Montgomery <montgomery.dean97@gmail.com>
2018-09-03 21:39:25 +0100

    different sw caching strategy

--- templates/sw.js
+++ templates/sw.js
@@ -1,9 +1,32 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js');
 
+workbox.skipWaiting();
+workbox.clientsClaim();
+
 workbox.core.setCacheNameDetails({
   prefix: 'stcs',
   suffix: 'v_{{ version }}'
 });
+
+// we can use this if we get https://github.com/GoogleChrome/workbox/issues/1612
+// workbox.precaching.precacheAndRoute([
+//   {% for k in manifest %}{url: '{{ k }}', revision: '{{ manifest[k] }}'},
+//   {% endfor %}
+// ]);
+// in the meantime we use this so we always get up to date data:
+self.addEventListener('install', event => {
+  const urls = [
+    {% for k in manifest %}
+    '{{ k }}',
+    {% endfor %}
+  ];
+  /* ... */
+  event.waitUntil(
+    caches.open(workbox.core.cacheNames.runtime).then(cache => cache.addAll(urls))
+  );
+});
+
+workbox.routing.setDefaultHandler(workbox.strategies.networkFirst({networkTimeoutSeconds: 2}));
 
 workbox.routing.registerRoute(
   /\.(?:png|gif|jpg|jpeg|svg)$/,
@@ -22,11 +45,3 @@ workbox.routing.registerRoute(
   new RegExp('^https://fonts.(?:googleapis|gstatic).com/(.*)'),
   workbox.strategies.cacheFirst(),
 );
-
-workbox.precaching.precacheAndRoute([
-  {% for k in manifest %}{url: '{{ k }}', revision: '{{ manifest[k] }}'},
-  {% endfor %}
-]);
-
-workbox.skipWaiting();
-workbox.clientsClaim();
