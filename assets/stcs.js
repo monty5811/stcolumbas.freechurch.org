64847b3170b12ca1706f73d15f03fb15d3033768
Dean Montgomery <montgomery.dean97@gmail.com>
2018-09-12 07:14:00 +0000

    switch to leaflet

--- assets/stcs.js
+++ assets/stcs.js
@@ -1,26 +1,43 @@
-const $ = require('jquery');
+const $ = require("jquery");
 window.$ = $;
 window.jQuery = $;
-const bs = require('bootstrap');
-const pay = require('./payments');
+const bs_transition = require("./node_modules/bootstrap/js/transition");
+const bs_collapse = require("./node_modules/bootstrap/js/collapse");
+const pay = require("./payments");
+
+function lMap() {
+  $(document).ready(function($) {
+    const url =
+      "https://www.google.co.uk/maps/place/St+Columba's+Free+Church+of+Scotland";
+    var map = L.map("map-container").setView(
+      [55.948757558814606, -3.194131548552832],
+      16
+    );
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([55.948757558814606, -3.194131548552832]).addTo(map);
+  });
+}
 
 function init() {
   // payments
   pay.init();
   // carousel
-  $(function() {
-    $('#carousel-stcs').carousel({
-      pause: 'false',
-    });
-  });
+  const mapElem = document.getElementById("map-container");
+  if (mapElem !== null) {
+    lMap();
+  }
 }
 
-document.addEventListener('DOMContentLoaded', function(event) {
+document.addEventListener("DOMContentLoaded", function(event) {
   init();
 });
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js');
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker.register("/sw.js");
   });
 }
