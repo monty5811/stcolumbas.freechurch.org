1aaeca6ef876c02ecd58be01fc07127023928f1e
Dean Montgomery <montgomery.dean97@gmail.com>
2018-09-30 14:56:23 +0100

    remove google maps

--- assets/stcs.js
+++ assets/stcs.js
@@ -4,41 +4,6 @@ window.jQuery = $;
 const bs = require('bootstrap');
 const pay = require('./payments');
 
-function gMap() {
-  $(document).ready(function($) {
-    const center = new google.maps.LatLng(55.948845, -3.194091999999955);
-    const map_pin = new google.maps.MarkerImage(
-      '/static/images/icons/mapmarker-icon-76x106.png'
-    );
-    map_pin.size = new google.maps.Size(38, 53);
-    map_pin.scaledSize = new google.maps.Size(38, 53);
-    map_pin.anchor = new google.maps.Point(38, 53);
-    const mapOptions = {
-      zoom: 15,
-      minZoom: 2,
-      maxZoom: 18,
-      center: center,
-      scrollwheel: false,
-      draggable: false,
-      mapTypeControl: true,
-      disableDefaultUI: false,
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-    };
-    const map = new google.maps.Map($('#map-container1')[0], mapOptions);
-    const title = '';
-    const churchMarker = new google.maps.Marker({
-      position: center,
-      map: map,
-      icon: map_pin,
-      url:
-        "https://www.google.co.uk/maps/place/St+Columba's+Free+Church+of+Scotland",
-    });
-    google.maps.event.addListener(churchMarker, 'click', function() {
-      window.open(this.url);
-    });
-  });
-}
-
 function init() {
   // payments
   pay.init();
@@ -48,11 +13,6 @@ function init() {
       pause: 'false',
     });
   });
-  // google map:
-  const mapElem = document.getElementById('map-container1');
-  if (mapElem !== null) {
-    gMap();
-  }
 }
 
 document.addEventListener('DOMContentLoaded', function(event) {
