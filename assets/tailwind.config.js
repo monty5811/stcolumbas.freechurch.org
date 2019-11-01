9c97556009705fa050c14eab938286206aeac599
Dean Montgomery <montgomery.dean97@gmail.com>
2019-11-01 00:10:24 +0000

    make menu float over content

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -87,6 +87,7 @@ module.exports = {
       "48": "12rem",
       "56": "14rem",
       "64": "16rem",
+      "96": "24rem",
       "128": "32rem"
     },
     backgroundColor: theme => theme("colors"),
