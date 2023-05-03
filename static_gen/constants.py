9b874481ba7fddf5af1fe51332484b3d488e06f1
Dean <monty5811@users.noreply.github.com>
2023-05-03 07:55:26 -0700

    Refresh site contents (#189)

--- static_gen/constants.py
+++ static_gen/constants.py
@@ -6,31 +6,3 @@ STATIC_DIR = "static"
 PUBLIC_DIR = "public"
 DIST_DIR = "dist"
 CACHE_DIR = ".image_cache"
-
-NavLink = namedtuple("NavLink", ["text", "href", "children"])
-
-NAV_LINKS = [
-    NavLink("Jesus", "/jesus", []),
-    NavLink(
-        "About",
-        None,
-        [
-            NavLink("Our Vision", "/about/our-vision", []),
-            NavLink("Our Team", "/about/our-team", []),
-            NavLink("Church Plants", "/about/church-plants", []),
-            NavLink("Contact Us", "/about/contact-us", []),
-        ],
-    ),
-    NavLink(
-        "Connect",
-        None,
-        [
-            NavLink("Visit", "/connect/visit", []),
-            NavLink("Community", "/connect/community", []),
-            NavLink("Giving", "/connect/giving", []),
-            NavLink("Ministries", "/connect/ministries", []),
-            NavLink("Serving", "/connect/serving", []),
-        ],
-    ),
-    NavLink("Updates", "/updates", []),
-]
